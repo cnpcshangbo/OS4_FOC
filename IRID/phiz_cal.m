013391c345c7fcfab883d8909cd074ad0879bac6
Bo Shang <cnpcshangbo@gmail.com>
2018-06-17 14:55:00 +0800

    pole zero matched method in FOPD

--- IRID/phiz_cal.m
+++ IRID/phiz_cal.m
@@ -8,7 +8,7 @@ cd=kp*(1+ki*dfod);
 s=tf('s');
 K=0.8592; T1=1.0710;
 p=K/(T1*s+1)/s;
-pd=c2d(p,Ts,'tustin');%plant
+pd=c2d(p,Ts,'matched');%plant
 
 phiz=cd*pd/(1+cd*pd);% this is to calculate the poles
 % this link show how poles determines the vibration: http://www.dcsc.tudelft.nl/~sc4060/transp/discreteNOTES.pdf
